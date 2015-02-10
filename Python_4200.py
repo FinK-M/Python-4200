9b6ef1c1d115cebe5eb1398690bb3fef31d9be77
Finlay Knops-Mckim <finlayknopsmckim@gmail.com>
2015-02-10 16:50:34 -0500

    Updated commands and script
    Updated commands to correctly perform voltage sweep and changed RPM
    switch commands to two wire mode. CVU results are now the same as when
    performed in KITE.
    Also started to add some error checking capability (try/except) to
    explain failures better.

--- Python_4200.py
+++ Python_4200.py
@@ -5,8 +5,17 @@ import csv
 
 
 def CV_output_san(values):
-    # Takes data string as proudced by KXCI command :CVU:OUTPUT:Z? and returns
-    # two arrays of decimal numbers representing the pairs of values receieved
+    """
+    ---------------------------------------------------------------------------
+    FUNCTION: CV_output_san
+    INPUTS: values (str)
+    RETURNS: prim, sec (list float)
+    DEPENDENCIES: none
+    ---------------------------------------------------------------------------
+    Takes data string as proudced by KXCI command :CVU:OUTPUT:Z? and returns
+    two arrays of decimal numbers representing the pairs of values receieved
+    ---------------------------------------------------------------------------
+    """
     values = (values.replace(";", ",").split(","))
     prim = [values[i] for i in range(0, len(values), 2)]
     sec = [float(values[i]) for i in range(1, len(values), 2)]
@@ -15,8 +24,17 @@ def CV_output_san(values):
 
 
 def select_device(rm):
-    # Takes a resource manager as an argument and lists available visa devices
-    # The user is then queried as to which device they would like to open
+    """
+    ---------------------------------------------------------------------------
+    FUNCTION: select_device
+    INPUTS: rm (visa.ResourceManager)
+    RETURNS: devices[selection] (str)
+    DEPENDENCIES: pyvisa/visa
+    ---------------------------------------------------------------------------
+    Takes a resource manager as an argument and lists available visa devices
+    The user is then queried as to which device they would like to open
+    ---------------------------------------------------------------------------
+    """
     devices = rm.list_resources()
 
     print("Found the following ", len(devices), " devices")
@@ -34,100 +52,137 @@ def select_device(rm):
 
 
 def csv_writer(prim, sec, ter):
-    # Takes two arrays of values and writes them to two columns in a csv file
-    # called "results.csv". The columns have headers "Input" and "Output"
+    """
+    ---------------------------------------------------------------------------
+    FUNCTION: csv_writer
+    INPUTS: values (str)
+    RETURNS: prim, sec (list float)
+    DEPENDENCIES: csv
+    ---------------------------------------------------------------------------
+    Takes two arrays of values and writes them to two columns in a csv file
+    called "results.csv". The columns have headers "Input" and "Output"
+    ---------------------------------------------------------------------------
+    """
     rows = zip(ter, prim, sec)
     with open('results.csv', 'w', newline='') as csvfile:
         reswrite = csv.writer(csvfile)
-        reswrite.writerow(["Y1", "Y2", "X1"])
+        reswrite.writerow(["X1", "Y1", "Y2"])
         for row in rows:
             reswrite.writerow(row)
 
 
-# create resource manager object
-rm = visa.ResourceManager()
+def dual_plot(x, x_label, y1, y1_label, y2, y2_label, x_min, x_max):
+    fig, ax1 = plt.subplots()
+    ax1.plot(x, y1, 'b')
+    ax1.set_xlabel(x_label)
+    ax1.set_ylabel(y1_label, color='b')
+    for tl in ax1.get_yticklabels():
+        tl.set_color('b')
 
-# pass resource manager to device selection prompt
-# comment to set device without prompt
-addr = select_device(rm)
+    ax2 = ax1.twinx()
+    ax2.plot(x, y2, 'r')
+    ax2.set_ylabel(y2_label, color='r')
+    for tl in ax2.get_yticklabels():
+        tl.set_color('r')
+    plt.xlim([x_min, x_max])
+    plt.grid(True)
+    plt.show()
 
-# addr = 'GPIB0::17::INSTR'
-# uncomment to set device without prompt
+proceed = None
+while not proceed:
+    try:
+        # create resource manager object
+        rm = visa.ResourceManager()
+    except:
+        print("Error: Cannot find visa list, please check configuration")
+        break
 
-# confirm address being used
-print("Using device at", addr)
+    # pass resource manager to device selection prompt
+    # comment to set device without prompt
+    addr = select_device(rm)
 
-# open visa resource at given address
-instr = rm.open_resource(addr)
+    # addr = 'GPIB0::17::INSTR'
+    # uncomment to set device without prompt
 
-# display the intrument's reported ID. Note the commant 'ID' is
-# specific to the Keithley 4200-SCS. Others may use '*IDN?'
-print("Instrument IDs as", instr.query('ID'))
+    # confirm address being used
+    print("Using device at", addr)
 
-# clear the visa resource buffers
-instr.clear()
+    # open visa resource at given address
+    try:
+        instr = rm.open_resource(addr)
+    except:
+        print("Error, cannot open resource")
+        break
 
-# send srq when finished with task
-instr.write('DR1')
-# access the user library page
-instr.write('UL')
-# run script to switch RPM1 to CVU mode
-instr.write('EX pmuulib kxci_rpm_switch(1,2)')
-# wait for script to complete
-instr.wait_for_srq()
-# print success
-print("done PMU1")
+    # display the intrument's reported ID. Note the commant 'ID' is
+    # specific to the Keithley 4200-SCS. Others may use '*IDN?'
+    print("Instrument IDs as", instr.query('ID'))
 
-# run script to switch RPM2 to CVU mode
-instr.write('EX pmuulib kxci_rpm_switch(2,2)')
-# wait for script to complete
-instr.wait_for_srq()
-# print success
-print("done PMU2")
+    # clear the visa resource buffers
+    instr.clear()
 
-# clear the buffer
-instr.write('BC')
+    # send srq when finished with task
+    instr.write('DR1')
+    # access the user library page
+    instr.write('UL')
+    # run script to switch RPM1 to CVU mode
+    instr.write('EX pmuulib kxci_rpm_switch(1,1)')
+    # wait for script to complete
+    instr.wait_for_srq()
+    # print success
+    print("Configured PMU1")
 
-# read commands file into a list
-with open('commands_fsweep.txt', 'r') as f:
-    commands = f.readlines()
+    # run script to switch RPM2 to CVU mode
+    instr.write('EX pmuulib kxci_rpm_switch(2,1)')
+    # wait for script to complete
+    try:
+        instr.wait_for_srq()
+    except:
+        print("Error: SRQ timeout")
+        break
 
-# parse list and send each item in turn to the instrument
-for command in commands:
-    instr.write(command)
+    # print success
+    print("Configured PMU2")
 
-print("Running tests...")
-instr.wait_for_srq()
-print("Done!")
+    # clear the buffer
+    instr.write('BC')
 
-instr.write(':CVU:DATA:Z?')
-values = str(instr.read_raw()).replace("b'", "").rstrip("'")
-values = values[:len(values) - 5]
+    # read commands file into a list
+    with open('commands_fsweep.txt', 'r') as f:
+        commands = f.readlines()
 
-instr.write(':CVU:DATA:FREQ?')
-volt = str(instr.read_raw()).replace("b'", "").rstrip("'")
-volt = volt[:len(volt)-5].split(",")
-volts = [float(v) for v in volt]
+    # parse list and send each item in turn to the instrument
+    for command in commands:
+        instr.write(command.rstrip('\n'))
 
-prim, sec = CV_output_san(values)
+    print("Running tests...")
+    instr.wait_for_srq()
+    print("Done!")
 
-prim = prim
-sec = sec
-volts = volts
+    instr.write(':CVU:DATA:Z?')
+    values = str(instr.read_raw()).replace("b'", "").rstrip("'")
+    values = values[:len(values) - 5]
 
-csv_writer(prim, sec, volt)
+    instr.write(':CVU:DATA:VOLT?')
+    volt = str(instr.read_raw()).replace("b'", "").rstrip("'")
+    volt = volt[:len(volt)-5].split(",")
+    volts = [float(v) for v in volt]
 
-fig, ax1 = plt.subplots()
-ax1.semilogx(volts, prim, 'b')
-ax1.set_xlabel('Frequency (Hz)')
-ax1.set_ylabel('Capacitance (F)', color='b')
-for tl in ax1.get_yticklabels():
-    tl.set_color('b')
+    prim, sec = CV_output_san(values)
 
-ax2 = ax1.twinx()
-ax2.semilogx(volt, sec, 'r')
-ax2.set_ylabel('Resistance (Ohms)', color='r')
-for tl in ax2.get_yticklabels():
-    tl.set_color('r')
+    prim = prim
+    sec = sec
+    volts = volts
+    total = [prim[i]*sec[i] for i in range(len(prim))]
 
-plt.show()
+    csv_writer(prim, sec, volt)
+    #plt.plot(volts, total)
+
+    dual_plot(volts, "Voltage (V)", prim, "Capacitance (F)",
+              sec, "Resistance", -5, 5)
+    """
+    print("Run again? (Y/N)")
+    if str(input("Run again? (Y/N): ")) == "N" or "n":
+        break
+    """
+    break
