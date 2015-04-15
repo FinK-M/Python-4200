0356ab2bb7142bc9b6eba8016747e39631b3dde7
Finlay Knops-Mckim <finlayknopsmckim@gmail.com>
2015-04-15 17:26:01 -0400

    Fixed freq_demo to run on actual hardware
    Changed program so that it runs smoothly in lab with GPIB output as
    opposed to just print statements.

--- freq_demo.py
+++ freq_demo.py
@@ -3,36 +3,33 @@ import csv
 from time import sleep
 from Python_4200 import select_device
 
+
 class routine(object):
 
     instances = 0
     routines = []
     csv_params = []
 
-    def __init__(self, name, instrument, delay = 1, steps = 5, low_f = 50, high_f = 100):
+    def __init__(self, name, delay=1, steps=5, low_f=50, high_f=100):
         routine.instances += 1
         self.name = name
-        self.instrument = instrument
         self.delay = delay
         self.steps = steps
         self.low_f = low_f
         self.high_f = high_f
-        self.params = [self.name, self.instrument, self.delay, self.steps, self.low_f, self.high_f]
-        routine.csv_params.append(self.params)
-        routine.routines.append(self)
 
     def parameters(self):
-         self.description = "{0}: delay {1} s, {2} steps, start f {3}Hz, end f {4}Hz".format(self.name, self.delay, self.steps, self.low_f, self.high_f)
-         print(self.description)
+        self.description = "{0}: delay {1} s, {2} steps, start f {3}Hz, end f {4}Hz".format(self.name, self.delay, self.steps, self.low_f, self.high_f)
+        print(self.description)
 
-    def set_delay(self, delay = 0):
-        
+    def set_delay(self, delay=0):
+
         if type(delay) != int and type(delay) != float:
             raise ValueError("Delay must be a float or int")
 
         if delay >= 1:
             self.delay = delay
-            
+
         else:
             while True:
                 try:
@@ -43,7 +40,7 @@ class routine(object):
                 except ValueError:
                     print("Please enter valid delay...")
 
-    def set_steps(self, steps = 0):
+    def set_steps(self, steps=0):
 
         if type(steps) != int:
             raise ValueError("Enter a postive integer number of steps")
@@ -61,14 +58,14 @@ class routine(object):
                 except ValueError:
                     print("Please enter valid number of steps...")
 
-    def set_low_f(self, low_f = 0):
+    def set_low_f(self, low_f=0):
 
         if type(low_f) != int:
             raise ValueError("Enter integer value between 40 and 300Hz")
-        
+
         if low_f >= 40 and low_f <= 300:
             self.low_f = low_f
-        
+
         else:
             while True:
                 try:
@@ -79,14 +76,14 @@ class routine(object):
                 except ValueError:
                     print("Please enter valid frequency...")
 
-    def set_high_f(self, high_f = 0):
+    def set_high_f(self, high_f=0):
 
         if type(high_f) != int:
             raise ValueError("Enter integer value between 100 and 400Hz")
-        
+
         if high_f >= 100 and high_f <= 400:
             self.high_f = high_f
-        
+
         else:
             while True:
                 try:
@@ -97,23 +94,30 @@ class routine(object):
                 except ValueError:
                     print("Please enter valid frequency...")
 
-    def run(self):
+    def run(self, instr):
         self.step_size = int((self.high_f - self.low_f)/self.steps)
         for f in range(int(self.low_f), int(self.high_f + 1), self.step_size):
             Vout = (f - 2.1333)/42.139
-            #self.instrument.write("VSET " + str(Vout))
+            instr.write("VSET " + str(Vout))
             print(f, "VSET " + str(Vout))
             sleep(self.delay)
-        #self.instrument.write("VSET 0")
+        instr.write("VSET 0")
+
+    def update(self):
+        self.params = [self.name, self.delay, self.steps, self.low_f, self.high_f]
+        routine.csv_params.append(self.params)
+        routine.routines.append(self)
 
 
-def create_routine(instr = "i"):
+def create_routine():
     name = input("Please enter the name for this routine: ")
-    r = routine(name, instr)
+    r = routine(name)
     r.set_delay()
     r.set_steps()
     r.set_low_f()
     r.set_high_f()
+    r.update()
+
 
 def save_routines():
     with open('settings.csv', 'w', newline='') as csvfile:
@@ -122,50 +126,55 @@ def save_routines():
             reswrite.writerow(row)
         csvfile.close()
 
+
 def open_routines():
-    with open('settings.csv', 'r', newline='') as csvfile:
-        resread = csv.reader(csvfile)
-        try:
+    try:
+        with open('settings.csv', 'r', newline='') as csvfile:
+            resread = csv.reader(csvfile)
             for row in resread:
-                r =(routine(row[0], "i"))
+                r = (routine(row[0], "i"))
                 r.name = row[0]
-                r.instrument = row[1]
-                r.delay = float(row[2])
-                r.steps = int(row[3])
-                r.low_f = int(row[4])
-                r.high_f = int(row[5])
-        except:
-            pass
-        csvfile.close()
+                r.delay = float(row[1])
+                r.steps = int(row[2])
+                r.low_f = int(row[3])
+                r.high_f = int(row[4])
+                r.update()
+            csvfile.close()
+    except:
+        print("No previous routines detected")
+
 
 if __name__ == "__main__":
-    """
+
     rm = visa.ResourceManager()
-    #addr = select_device(rm)
-    addr = 'GPIB0::3::INSTR'
+    addr = select_device(rm)
     instr = rm.open_resource(addr)
-    """
-    instr = "i"
-    
+    instr.clear()
+    sleep(1)
+    instr.write("VSET 1")
     open_routines()
+    while True:
+        i = 0
+        for r in routine.routines:
+            print(i, ": ", end='')
+            r.parameters()
+            i += 1
 
-    i = 0
-    for r in routine.routines:
-        print(i, ": ", end='')
-        r.parameters()
-        i += 1
-    
+        if i == 0:
+            print("Please create a first routine")
+            selection = 0
+            create_routine()
+        else:
+            print(i, ": Create new routine")
+            selection = int(input("Make a selection: "))
+            if selection == i:
+                create_routine()
 
-    if i == 0:
-        print("Please create a first routine")
-        selection = 0
-        create_routine(instr)
-    else:
-        print(i, ": Create new routine")
-        selection = int(input("Make a selection: "))
-        if selection == i:
-            create_routine(instr)
+        active = routine.routines[selection]
+        active.run(instr)
 
-    active = routine.routines[selection]
-    active.run()
+        if input("Exit? (y/n) : ").lower() == "y":
+            break
+
     save_routines()
+    instr.close()
